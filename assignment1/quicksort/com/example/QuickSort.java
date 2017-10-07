6c7c0e095d33e037f479ac0a3298e114408e6b96
Michael Scales <mscales@mindwireless.com>
2017-10-07 13:21:47 -0500

    Add first files.
    Hellworld forquick sort

---
+++ assignment1/quicksort/com/example/QuickSort.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
