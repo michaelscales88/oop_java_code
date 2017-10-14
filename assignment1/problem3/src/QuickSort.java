efedaabbd0798dc6de3245471baacc1ddae8433d
Michael Scales <mscales@mindwireless.com>
2017-10-14 10:48:14 -0500

    Add basic quicksort
    Only works for small sets. Over a certain size it breaks on stack
    overflow.

--- assignment1/problem3/src/QuickSort.java
+++ assignment1/problem3/src/QuickSort.java
@@ -2,53 +2,62 @@ package src;
 
 public class QuickSort {
 
-    private int[] my_numbers;
-    int size;
+    private String file_name;
+    private int size;
 
     QuickSort(String file_name, int size)
     {
         this.size = size;
-        my_numbers = FileOpener.open_file(file_name, size);
+        this.file_name = file_name;
     }
 
-    static void print(int[] numbers)
+    private void print(int[] my_numbers)
     {
-        for (int number : numbers) {
+        System.out.print("My numbers: [ ");
+        for (int number : my_numbers) {
             System.out.print(number + " ");
         }
+        System.out.println("]");
     }
 
-    void partition(int[] numbers)
-    {
-        int pivot_idx = size / 2,
-            rightmark = size,
-            leftmark = 0,
-            tmp;
+    void quick_sort(int[] my_numbers, int left, int right) {
+        if (my_numbers == null || my_numbers.length == 0) return;
+        if (left >= right) return;
+        int pivot_idx = (left + (right - left)) / 2;
+        int i = left, j = right;
+        while (i <= j)
+        {
+            // Find where value larger than pivot
+            while (my_numbers[i] < my_numbers[pivot_idx]) i++;
+            // Find where the lower value than pivot
+            while (my_numbers[j] > my_numbers[pivot_idx]) j--;
 
-        // Track partition
-        while (leftmark < rightmark) {
-            // Find larger number on left
-            while (numbers[leftmark] < numbers[pivot_idx]) { leftmark++; }
-            // Find larger number on right
-            while (numbers[rightmark] < numbers[pivot_idx]) { rightmark--; }
-
+            if (i <= j)
+            {
+                int temp = my_numbers[i];
+                my_numbers[i] = my_numbers[j];
+                my_numbers[j] = temp;
+                i++;
+                j--;
+            }
         }
+        if (left < j)
+            quick_sort(my_numbers, left, j);
 
-        QuickSort.print(numbers);
+        if (right > i)
+            quick_sort(my_numbers, i, right);
     }
 
-    void sort_asc() {
-        int pivot_idx = 0;
-
-//        for (;pivot_idx < 20; pivot_idx++) {
-//            System.out.println(numbers[pivot_idx]);
-//        }
-        QuickSort.print(this.my_numbers);
+    void sort_asc()
+    {
+        int[] my_numbers = FileOpener.open_file(file_name, size);
+        print(my_numbers);
+        quick_sort(my_numbers,0, size - 1);
+        print(my_numbers);
     }
 
     public static void main(String[] args) {
         QuickSort s1 = new QuickSort(args[0], Integer.parseInt(args[1]));
         s1.sort_asc();
-//        QuickSort.partition(numbers);
     }
 }
