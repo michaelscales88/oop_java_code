31087773e145cf36cc945912f4f0a099a911f439
Matthew Wiederaenders <matthew.wiederaenders@g.austincc.edu>
2017-10-16 17:20:06 -0500

    Matrix rough draft compiles with menu.

---
+++ assignment1/quadratic/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd '/Users/matthewwiederaenders/Documents/Course Documents/CS 3354/p1/oop_java_code/assignment1/quadratic/'
+javac Quadratic.java Complex.java
+cd ..
+java quadratic.Quadratic 10.0 1.0 34.0
