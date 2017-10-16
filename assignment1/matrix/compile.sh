e1f6b97e132d74531a4332ab9c821ba9c8a5d03f
Matthew Wiederaenders <matthew.wiederaenders@g.austincc.edu>
2017-10-16 18:09:06 -0500

    Updated menu, gives error message before creating and defining matrix sizes.

---
+++ assignment1/matrix/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd '/Users/matthewwiederaenders/Documents/Course Documents/CS 3354/p1/oop_java_code/assignment1/matrix/'
+javac MatrixMath.java Matrix.java MyException.java
+cd ..
+java matrix.MatrixMath
