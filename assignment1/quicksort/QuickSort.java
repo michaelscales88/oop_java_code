cc5cc556534bae141da1fd7f3eb5e84d9b8b0a0b
Michael Scales <mscales@mindwireless.com>
2017-10-30 12:22:52 -0500

    Add second implementation of quicksort and timing.
    Added quicksort algorithm without partition and added timers to
    demonstrate the speed difference between implementations.

--- assignment1/quicksort/QuickSort.java
+++ assignment1/quicksort/QuickSort.java
@@ -31,17 +31,51 @@ public class QuickSort
       }
    }
 
-   private void quick_sort(int left, int right)
+
+   private void p_quick_sort(int left, int right)
    {
       if (left < right)
       {
          int mid = partition(left, right);
-         quick_sort(left, mid);
-         quick_sort(mid + 1, right);
+         p_quick_sort(left, mid);
+         p_quick_sort(mid + 1, right);
       }
    }
 
-   public void sort_asc(int[] numbers)
+   private void quick_sort(int left, int right)
+   {
+      int i = left, j = right;
+      int pivot = my_nums[left + (right - left) / 2];
+
+      while (i <= j)
+      {
+         while (my_nums[i] < pivot) i++;
+         while (my_nums[j] > pivot) j--;
+
+         if (i <= j)
+         {
+            swap(i, j);
+            i++;
+            j--;
+         }
+      }
+
+      if (left < j)
+         quick_sort(left, j);
+      if (right > i)
+         quick_sort(i, right);
+   }
+
+   public void partition_sort(int[] numbers)
+   {
+      if (numbers == null || numbers.length == 0) return;
+      my_nums = numbers;
+      printer.print(my_nums);
+      p_quick_sort(0, my_nums.length - 1);
+      printer.print(my_nums);
+   }
+
+   public void no_partition_sort(int[] numbers)
    {
       if (numbers == null || numbers.length == 0) return;
       my_nums = numbers;
@@ -65,12 +99,19 @@ public class QuickSort
       // Parse CLI arguments
       int[] numbers = FileOpener.open_file(args[0], Integer.parseInt(args[1]));
 
-      // Test sort ascending
-      start_time = System.nanoTime();
-      s1.sort_asc(numbers);
-      end_time = System.nanoTime();
+      // Test sort ascending partition
+      start_time = System.currentTimeMillis();
+      s1.no_partition_sort(numbers);
+      end_time = System.currentTimeMillis();
       duration = (end_time - start_time);
-      System.out.println("Duration: " + duration / 1000000 + " ms");
+      System.out.println("Duration: " + duration + " ms");
+
+      // Test sort ascending partition
+      start_time = System.currentTimeMillis();
+      s1.partition_sort(numbers);
+      end_time = System.currentTimeMillis();
+      duration = (end_time - start_time);
+      System.out.println("Duration: " + duration + " ms");
 
       LOGGER.info("Logging ended.");
       // Close log file
